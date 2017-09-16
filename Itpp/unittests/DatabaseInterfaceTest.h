af9e0320cccd74ae919131fce2f5501867e5a052
Your Name <you@example.com>
2017-09-16 10:30:19 +0200

    Adds UT of DatabaseInterface.cpp

---
+++ Itpp/unittests/DatabaseInterfaceTest.h
@@ -0,0 +1,39 @@
+#include "DatabaseInterface.h"
+
+#include <cxxtest/TestSuite.h>
+#include <assert.h>
+
+
+class DatabaseInterfaceTestSuite: public CxxTest::TestSuite {
+
+	DatabaseInterface *m_pDatabaseInterface;
+	tinyxml2::XMLDocument m_xmlDoc;
+	tinyxml2::XMLElement *m_xmlRoot;
+
+public:
+
+	void setUp() {
+		m_pDatabaseInterface = new DatabaseInterface("unittest_databaseinterface.xml");
+	}
+
+	void tearDown() {
+
+		delete m_pDatabaseInterface;
+	}
+
+	void testGetSoftwareArchitectureDocumentationElementTextTitle() {
+		std::string sTitle = m_pDatabaseInterface->GetSoftwareArchitectureDocumentationElementText(1, "Title");
+		TS_ASSERT_EQUALS(sTitle, "First Title");
+	}
+
+	void testGetSoftwareArchitectureDocumentationElementSecondTitle() {
+		std::string sTitle = m_pDatabaseInterface->GetSoftwareArchitectureDocumentationElementText(2, "Title");
+		TS_ASSERT_EQUALS(sTitle, "Second title");
+	}
+	void testGetSoftwareArchitectureDocumentationElementTextNotExist() {
+		std::string sTitle = m_pDatabaseInterface->GetSoftwareArchitectureDocumentationElementText(1, "DOES_NOT_EXIST");
+		TS_ASSERT_EQUALS(sTitle, "");
+	}
+
+	// TODO V Test with XML file not exist.
+};
